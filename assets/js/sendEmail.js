e0a65ad9a0573f4b26d24464b3643f66eefc2a0b
Cosmin Timofte <timoftecosmin@yahoo.com>
2024-05-08 09:18:28 +0000

    Implement emails integration on contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,17 @@
+function sendMail(contactForm) {
+    var templateParams = {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+      };
+      
+      emailjs.send("service_dz6rzqc","template_th0uhfp", templateParams).then(
+        (response) => {
+          console.log('SUCCESS!', response.status, response.text);
+        },
+        (error) => {
+          console.log('FAILED...', error);
+        },
+      );
+      return false;
+}
