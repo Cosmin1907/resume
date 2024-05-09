d2fdd861b6e94fbc3d0dc1e108658096a5597759
Cosmin Timofte <timoftecosmin@yahoo.com>
2024-05-09 06:13:57 +0000

    Add work around EmailJs issue

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,7 +1,7 @@
 function sendMail(contactForm) {
     var templateParams = {
         "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
+        "email": contactForm.emailaddress.value,
         "project_request": contactForm.projectsummary.value
       };
       
